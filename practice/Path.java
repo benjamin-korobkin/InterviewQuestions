908ef81e9331cea89dc72c2ad0c26116c32a6825
Benjamin Korobkin <22179668+bjklol@users.noreply.github.com>
2018-01-10 15:50:30 -0500

    Add files via upload

--- practice/Path.java
+++ practice/Path.java
@@ -1,6 +1,9 @@
 // questions on testdome.
-
 package practice;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
 
 public class Path 
 {
@@ -18,13 +21,43 @@ public class Path
 
     public void cd(String newPath) 
     {
-        if (newPath.isEmpty()) path = "/";
+        if (newPath.equals("/") || newPath.isEmpty()) path = "/";
         else
-        {
+        {   // put each part of the newPath, excluding slashes, into an arrayDeque.
+        	String[] manip = newPath.split("/");
+        	List<String> list = Arrays.asList(manip);
+        	ArrayDeque<String> newDeque = new ArrayDeque<String>(list);
+        	if(newDeque.getFirst().isEmpty()) newDeque.removeFirst(); // remove blank at the front, if exists.
+        	System.out.println(newDeque); // In example, prints [.., x]
         	
+        	// Same with the original path.
+        	manip = path.split("/");
+        	list = Arrays.asList(manip);
+        	ArrayDeque<String> pathDeque = new ArrayDeque<String>(list);
+        	if(pathDeque.getFirst().isEmpty()) pathDeque.removeFirst(); // remove blank at front, if exists.
+        	System.out.println(pathDeque); // In example, prints [a, b, c, d]
+        	
+        	// we can now manipulate both the path and newPath strings.
+        	while(!newDeque.isEmpty())
+        	{
+        		if (newDeque.getFirst().equals(".."))
+        		{
+        			pathDeque.removeLast();
+        		}
+        		else
+        		{
+        			pathDeque.addLast(newDeque.getFirst());
+        		}
+        		newDeque.removeFirst();
+        	}
+        	
+        	
+        	path = "";
+        	while (!pathDeque.isEmpty())
+        	{
+        		path += "/" + pathDeque.removeFirst();
+        	}
         }
-        
-        
     }
 
     public static void main(String[] args) 
